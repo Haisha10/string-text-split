01abed832a8595d81960225cb7013ef2209986b6
AlvaroL <u202012770@upc.edu.pe>
2023-01-17 20:47:02 -0500

    add light/dark theme and split fixes

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
 function splitString() {
-    var inputString = document.getElementById("string").value;
-    var delimiter = "";
-    var keepDelimiter = false;
-    if (document.getElementById("commaSpace").checked) {
-      delimiter = /[\s,]+/g;
-      if (document.getElementById("yes").checked) {
-        var output = inputString.replace(delimiter, '$&\n');
-      } else {
-        var output = inputString.replace(delimiter, '\n');
-      }
-    } else if (document.getElementById("comma").checked) {
-      delimiter = ",";
-      var splitString = inputString.split(delimiter);
-      var output = splitString.join('\n');
-    } else if (document.getElementById("space").checked) {
-      delimiter = " ";
-      var splitString = inputString.split(delimiter);
-      var output = splitString.join('\n');
-    } else if (document.getElementById("other").checked) {
-      delimiter = document.getElementById("otherDelimiter").value;
-      var splitString = inputString.split(delimiter);
-      var output = splitString.join('\n');
-    }
-    if (document.getElementById("yes").checked) {
-      keepDelimiter = true;
-    }
-    document.getElementById("output").value = output;
+  var inputString = document.getElementById("string").value;
+
+  if (document.getElementById("comma").checked) {
+    var delimiter = ",";
+  }
+  else if (document.getElementById("space").checked) {
+    var delimiter = " ";
+  }
+  else if (document.getElementById("other").checked) {
+    var delimiter = document.getElementById("otherDelimiter").value;
+  }
+
+  if (document.getElementById("trimYes").checked) {
+    var splitString = inputString.split(delimiter).map(element => element.trim());
+  }
+  else if (document.getElementById("trimLeft").checked) {
+    var splitString = inputString.split(delimiter).map(element => element.trimLeft());
+  }
+  else if (document.getElementById("trimRight").checked) {
+    var splitString = inputString.split(delimiter).map(element => element.trimRight());
+  }
+  else if (document.getElementById("trimNo").checked) {
+    var splitString = inputString.split(delimiter);
+  }
+
+  if (document.getElementById("delimiterYes").checked) {
+    var output = splitString.join(delimiter + '\n');
+  }
+  else if (document.getElementById("delimiterNo").checked) {
+    var output = splitString.join('\n');
+  }
+  document.getElementById("output").value = output;
+}
+
+let theme = "light";
+function toggleTheme() {
+  if (document.getElementById("switch-knob").style.left === "0px") {
+    document.getElementById("switch-knob").style.left = "25px";
+    document.getElementById("theme").href = "dark.css";
+    theme = "dark";
+  } else {
+    document.getElementById("switch-knob").style.left = "0px";
+    document.getElementById("theme").href = "light.css";
+    theme = "light";
+  }
 }
